0c86acf75ef7f325b7b6bbd2e109d3d36bcbe388
Cauê Lima <cauesvlima@gmail.com>
2024-10-10 22:06:50 -0300

    dotenvano a parada

---
+++ todolist/env.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      VITE_PORT: string; // Defina todas as variáveis de ambiente que você precisa
+    }
+  }
+  
--- todolist/vite.config.ts
+++ todolist/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config(); // Carrega as variáveis do arquivo .env
+
+// Declara a variável config com o tipo string ou undefined
+const config: string | undefined = process.env.VITE_PORT;
+
+// Converte a variável config para número, garantindo que tenha um valor
+const port: number = parseInt(config || '5173', 10); // Usa 5173 como padrão se config estiver indefinido
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port, // Usa a variável port
+  },
+});
