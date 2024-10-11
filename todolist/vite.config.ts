96d36e88989224878039b11d9f909e408d323e39
Cauê Lima <cauesvlima@gmail.com>
2024-10-10 22:23:22 -0300

    favicon

--- todolist/vite.config.ts
+++ todolist/vite.config.ts
@@ -1,18 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import * as dotenv from 'dotenv';
-
-dotenv.config(); // Carrega as variáveis do arquivo .env
-
-// Declara a variável config com o tipo string ou undefined
-const config: string | undefined = process.env.VITE_PORT;
-
-// Converte a variável config para número, garantindo que tenha um valor
-const port: number = parseInt(config || '5173', 10); // Usa 5173 como padrão se config estiver indefinido
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port, // Usa a variável port
-  },
-});
+  server:{
+    port:8000,
+  }
+})
