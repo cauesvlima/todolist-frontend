0cfc6d8179a6f77e592900873d57da2329810d6e
Cauê Lima <cauesvlima@gmail.com>
2024-10-14 18:10:08 -0300

    colocando valores corretos

--- todolist/src/components/lists/index.tsx
+++ todolist/src/components/lists/index.tsx
@@ -1,39 +1,62 @@
 import styles from './styles.module.scss';
 import { useListServices } from '../../services/list';
 import { useEffect, useState } from 'react';
-export const Lists = () =>{
-    const{listGetAll} = useListServices();
-    const [lists, setLists] = useState();
 
-    const handleViewLists= async()=>{
+interface Task {
+    id: number;
+    name: string;
+}
+
+interface List {
+    id: number;
+    nome: string;
+    tasks: Task[];
+}
+
+export const Lists = () => {
+    const { listGetAll } = useListServices();
+    const [lists, setLists] = useState<List[]>([]);
+    const [editName, setEditName] = useState(0);
+
+    const handleViewLists = async () => {
         const response = await listGetAll();
         setLists(response.data.listas);
         console.log(`Total de tarefas: ${response.data.listas[0].tasks.length}`);
         console.log(response.data.listas);
     };
 
+    const viewEdit = (id:number) =>{
+        setEditName(id);
+    }
 
-    useEffect(()=>{
+    useEffect(() => {
         handleViewLists();
-    },[]);
-    
-return(
-    <div>
-        <div className={styles.listHeader}>
-            <h3>
-            Listas
-            </h3>
-        </div>
-        <div className={styles.listBody}>
-            <div className={styles.listItem}>
-                <div className={styles.listName}>
-                    Nome da tarefa
-                </div>
-                <div className={styles.listAmount}>
-                    Quantidade de tarefas-0
-                </div>
+    }, []);
+
+    return (
+        <div>
+            <div className={styles.listHeader}>
+                <h3>Listas</h3>
+            </div>
+            <div className={styles.listBody}>
+                {lists.length > 0 ? (
+                    lists.map((list) => (
+                        <div className={styles.listItem} onMouseEnter={() => viewEdit(list.id)} onMouseLeave={() => viewEdit(0)}  key={list.id}>
+                            <div className={styles.listName}>
+                                {list.nome}
+                                {editName=== list.id && (
+                                    <span>oi</span>
+                                )}
+                            </div>
+                            <div className={styles.listAmount}>
+                                Quantidade de tarefas: <span>{list.tasks.length}</span>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>Nenhuma lista encontrada.</p> /* Mensagem para caso não haja listas */
+                )}
             </div>
         </div>
-    </div>
-)
-}
+    );
+};
